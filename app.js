22a188587873fb4fc76d46a4a6df7aa0b0f0df34
Leonardo Coronel <leomcoronel99@gmail.com>
2020-09-01 22:45:24 -0300

    Actualización
    Agrego las funcionalidades para cambiar el tamaño de la caja, así poder cambiar las formas desde un botón hasta un rectángulo y poder previsualizar el border-radius

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 // obtener datos de inputs
 const box = document.getElementById("box");
+
 let input1 = document.getElementById("top-left").addEventListener("input", updateRadius);
 let input2 = document.getElementById("top-right").addEventListener("input", updateRadius);
 let input3 = document.getElementById("bottom-left").addEventListener("input", updateRadius);
 let input4 = document.getElementById("bottom-right").addEventListener("input", updateRadius);
+
 const copyBtn = document.getElementById("copyBtn");
+
+// Width and height
+
+let width = document.getElementById("width").addEventListener("input", updateWidthAndHeight);
+let height = document.getElementById("height").addEventListener("input", updateWidthAndHeight);
+
+// Funcion para update width y height
+
+function updateWidthAndHeight() {
+  let width = document.getElementById("width").value;
+  let height = document.getElementById("height").value;
+
+  // console.log(width, height);
+
+  let ancho = box.style.width = `${width}px`;
+  let alto = box.style.height = `${height}px`;
+}
 
 // Se realiza el update del radius de la caja
 function updateRadius() {
